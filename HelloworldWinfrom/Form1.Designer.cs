057e748091e100867664e079b227ace529ba0385
iris-starry <iris_starry_@naver.com>
2024-03-22 11:22:07 +0900

    C-sharp Winfrom 실습

--- HelloworldWinfrom/Form1.Designer.cs
+++ HelloworldWinfrom/Form1.Designer.cs
@@ -28,13 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("궁서", 25F);
+            this.label1.Location = new System.Drawing.Point(79, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(636, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "3-1 급훈 : 아가씨들 취업할 시간입니다.";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.8D;
+            this.Text = "3102 김선희 안녕 세상아!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- HelloworldWinfrom/Form1.cs
+++ HelloworldWinfrom/Form1.cs
@@ -16,5 +16,10 @@ namespace HelloworldWinfrom
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
